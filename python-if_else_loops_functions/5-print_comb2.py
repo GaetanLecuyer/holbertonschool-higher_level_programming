f5c74707ee5f621b3fee8791e852212e2bc71c14
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-05-31 15:23:09 +0200

    remove the code in order to conver char on interger

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i == 99:
-        print("{}".format(i), end="\n")
-    print("{},".format(i), end=" ")
+for i in range(0, 99):
+    print("{:02d}, ".format(i), end="")
+    i = i+1
+print("{:02d}".format(i))
