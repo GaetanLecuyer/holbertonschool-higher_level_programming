fd01857cbedf95fed634859aa7c703fce9143b41
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-05-31 16:19:58 +0200

    add file combinaison

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-for i in range(10):
+for i in range(0, 8):
     for j in range(i + 1, 10):
-        if i == 8 or i == 9:
-            print("{}{}".format(i, j), end=" ")
-        else:
-            print("{}{},".format(i, j), end=" ")
-print()
+        print("{}{}, ".format(i, j), end="")
+print("89")
