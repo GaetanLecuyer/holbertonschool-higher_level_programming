4943303e72989496a1ab53f1f4eebe0655d193f8
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-05-31 12:14:33 +0200

    chaine orienter objet

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class O: __str__=lambda _: "object-oriented"
+class P: __str__=lambda _: "programming"
+class Y: __str__=lambda _: "with"
+class T: __str__=lambda _: "Python"
+print(O(),P(),Y(),T())
