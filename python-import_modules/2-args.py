03340ba2e0fb21c75cb491aa07dfa74732c62388
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-06-01 14:22:31 +0200

    add the executable

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-import sys
-
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        print("0 arguments.")
-    elif len(sys.argv) == 2:
-        print("1 argument:")
-        print("1: {}".format(sys.argv[1]))
+    from sys import argv
+
+    if len(argv) <= 1:
+        print("{} arguments.".format(len(argv) - 1))
+    elif len(argv) == 2:
+        print("{} argument:".format(len(argv) - 1))
     else:
-        print("{} arguments:".format(len(sys.argv) - 1))
-        for i in range(1, len(sys.argv)):
-            print("{}: {}".format(i, sys.argv[i]))
+        print("{} arguments:".format(len(argv) - 1))
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
