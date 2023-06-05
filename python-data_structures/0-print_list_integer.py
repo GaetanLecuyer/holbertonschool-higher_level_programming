62c9fe7ae596faa5b3abdb4b8b27c59bc2d80d2a
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-06-05 15:08:17 +0200

    mofication for styl

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    if my_list is None:
-        return -1
-    
-    for item in my_list:
-        print(item)
+    for i in my_list:
+        print("{:d}".format(i))
