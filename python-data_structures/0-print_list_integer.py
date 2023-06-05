402a2881419532e8310d5fa85f58e2c03dae9458
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-06-05 15:02:47 +0200

    add if liste is NULL

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    i = 0
+    if my_list is None:
+        return -1
+    
     for item in my_list:
         print(item)
-        i = i + 1
