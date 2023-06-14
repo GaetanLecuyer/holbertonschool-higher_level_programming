d5694d4a7c7ea6f2bdbb0bd1fe1297e2d98337d9
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-06-14 14:19:13 +0200

    class square

---
+++ python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self):
+        """
+        Constructeur de la classe Square.
+        Cette methode est appelee lorsqu'une instance de la classe est cree.
+        Elle ne prend aucun argument supplementaire en dehors de 'self',
+        qui represente l'instance actuelle de la classe.
+        """
+        pass
