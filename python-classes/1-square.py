6cf33dd8f1431e6fed2f224a7d2e0a4efe7d5a1a
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-06-14 14:36:56 +0200

    modification pythonStyle comment:remove space  file: 1-square

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -6,12 +6,9 @@ class Square:
 """represente and square class"""
 
     def __init__(self, size):
-        """
-        Constructor for the Square class.
-        Initializes a new instance of Square with the given size.
+        """Initializes a new instance of Square with the given size.
 
         Args:
             size (int): The size of the square.
-
         """
         self.__size = size
