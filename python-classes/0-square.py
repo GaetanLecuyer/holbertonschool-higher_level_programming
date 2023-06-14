355c296123ad5d4a720190867aa94129b3131215
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-06-14 14:25:11 +0200

    modification sythonStyle file 0-square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
+"""Define a class Square."""
+
+
 class Square:
-    def __init__(self):
-        """
-        Constructor for the Square class.
-        This method is called when an instance of the class is created.
-        It takes no additional arguments besides 'self',
-        which represents the current instance of the class.
-        """
-        pass
+    """Represent a square."""
+    pass
