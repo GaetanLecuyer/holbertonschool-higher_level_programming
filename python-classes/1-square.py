646c92b55aeee1c99d0a2654936f34055a6eb9dd
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-06-14 14:31:33 +0200

    create the atrbus a class square file: 1-square

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""square is a class"""
+
+class Square:
+    def __init__(self, size):
+        """
+        Constructor for the Square class.
+        Initializes a new instance of Square with the given size.
+
+        Args:
+            size (int): The size of the square.
+
+        """
+        self.__size = size
