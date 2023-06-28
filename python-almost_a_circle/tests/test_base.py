5c1bb4796e2183d938baf95f2b02ded96a16e277
GaetanLecuyer <6229@holbertonstudents.com>
2023-06-28 15:19:38 +0200

    send file test

---
+++ python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+"""Define a class Base."""
+
+import json
+
+
+class Base:
+    """Represent the base model.
+
+    Represents the "base" for all other classes in project 0x0C*.
+
+    Attributes:
+        __nb_objects (int): The number of instantiated Bases.
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON serialization of a list of dicts."""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file"""
+        filename = cls.__name__ + ".json"
+        if list_objs is None:
+            list_objs = []
+        dict_list = [obj.to_dictionary() for obj in list_objs]
+        json_string = cls.to_json_string(dict_list)
+        with open(filename, 'w') as file:
+            file.write(json_string)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the deserialization of a JSON string."""
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Return a class instantied from a dictionary of attributes."""
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                new = cls(1, 1)
+            else:
+                new = cls(1)
+            new.update(**dictionary)
+            return new
+
+    @classmethod
+    def load_from_file(cls):
+        """Return a list of classes from a file of JSON strings."""
+        filename = cls.__name__ + ".json"
+        instances = []
+
+        try:
+            with open(filename, 'r') as file:
+                json_string = file.read()
+                dictionaries = Base.from_json_string(json_string)
+
+                for dictionary in dictionaries:
+                    instance = cls.create(**dictionary)
+                    instances.append(instance)
+
+        except FileNotFoundError:
+            return []
+
+        return instances
