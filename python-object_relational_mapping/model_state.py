6b5f7acd42faef8ce70d46e1ce71d39655cafd38
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-07-31 16:04:59 +0200

    add the task 6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,36 +1,20 @@
-from sqlalchemy import Column, Integer, String, create_engine
+#!/usr/bin/python3
+# Defines a State model.
+# Inherits from SQLAlchemy Base and links to the MySQL table states.
+
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-# Create a declarative base instance
 Base = declarative_base()
 
+
 class State(Base):
-    """
-    Represents a State table in the database.
-    """
+    """Represents a state for a MySQL database.
 
-    __tablename__ = 'states'  # The name of the MySQL table
-
-    # Columns definition
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    __tablename__ (str): The name of the MySQL table to store States.
+    id (sqlalchemy.Integer): The state's id.
+    name (sqlalchemy.String): The state's name.
+    """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-
-if __name__ == "__main__":
-    # This part is only executed when the script is run directly
-
-    # Replace 'your_mysql_username' and 'your_mysql_password' with your actual MySQL credentials
-    mysql_username = 'your_mysql_username'
-    mysql_password = 'your_mysql_password'
-    database_name = 'your_database_name'  # Replace with your actual database name
-
-    # Define the connection string to connect to MySQL running on localhost at port 3306
-    connection_string = f"mysql+mysqldb://{mysql_username}:{mysql_password}@localhost:3306/{database_name}"
-
-    # Create the engine to connect to the database
-    engine = create_engine(connection_string)
-
-    # Import all classes that inherit from Base before calling Base.metadata.create_all()
-    from other_module import SomeOtherClass  # Import other classes if you have them
-
-    # Create all tables defined in the Base
-    Base.metadata.create_all(engine)
